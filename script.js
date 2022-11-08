4e97904d3b0ac794c93cede9b75246f0dfc06d8d
poojatiwari191 <117286071+poojatiwari191@users.noreply.github.com>
2022-11-08 16:43:00 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+<script>
 
+function __doPostBack(eventTarget, eventArgument) {
+    if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+        theForm.__EVENTTARGET.value = eventTarget;
+        theForm.__EVENTARGUMENT.value = eventArgument;
+        theForm.submit();
+    }
+}
+</script>
